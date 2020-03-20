b08ed6081360b6695abfcca8708a88c6df24344a
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2020-03-20 13:20:05 +0900

    Implement songs#new and songs#create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
 
+  private
+
   def current_user
     @current_user ||= if session[:user_id]
                         User.find_by(id: session[:user_id])
                       else
                         nil
                       end
+  end
+
+  def require_current_user
+    redirect_to login_path, alert: 'ログインしてください' if current_user.nil?
   end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,26 @@
 class SongsController < ApplicationController
+  before_action :require_current_user, only: %i[new create]
+
   def index
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params.merge(user: current_user))
+
+    if @song.save
+      redirect_to root_path, notice: '曲を追加しました'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:name, :artist, :url, :notes)
   end
 end
