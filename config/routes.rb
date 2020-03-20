fb080976ce494deed7de7690fa795ccc851b431d
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2020-03-20 12:18:46 +0900

    Implement users#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   root 'auth0#login'
   get 'auth/auth0/callback' => 'auth0#callback'
   get 'auth/failure' => 'auth0#failure'
+
+  resources :users, only: :index
+
+  direct :livelog_member do |livelog_member_id|
+    "https://livelog.ku-unplugged.net/members/#{livelog_member_id}"
+  end
 end
